dedd7aa25c7993488213355b85fb356f94fd7619
Luix70 <lortuno1@gmail.com>
2019-10-07 18:28:12 +0200

    from laptop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "./App.css";
-import Tablaclientes from "./components/Tablaclientes";
+import Tablaclientes from "./components/Tablaclientes.jsx";
 function App() {
   return (
     <main>
