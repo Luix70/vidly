97afc86938525a491fff356e552b7e50c9ead0f3
Luix70 <lortuno1@gmail.com>
2019-10-08 10:03:07 +0200

    minor changes

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,45 +1,42 @@
-import React, { Component } from "react";
+import React from "react";
+// al ser una SFC no se requiere importar Component
 import Operacion from "./Operacion";
-const Cliente = ({cliente:cli, onDelete}) => {
-    return ( 
+const Cliente = ({ cliente: cli, onDelete }) => {
+  return (
     <React.Fragment>
-        <tr key={cli.codigo}>
-          <td>
-            <strong>{cli.codigo}</strong>
-          </td>
-          <td>
-            <em> {cli.rzs}</em>
-          </td>
-          <td className="numped">{cli.totalDocumentos}</td>
-          <td className="numped">
-            <button
-              className="btn btn-danger btn-sm"
-              onClick={() => onDelete(cli.codigo)}
-            >
-              borrar
-            </button>
-          </td>
-        </tr>
-        <tr key={cli.codigo + "-ops"}>
-          <td>&nbsp;</td>
-          <td colSpan="3">
-            <table className="table table-dark table-sm">
-              <thead></thead>
-              <tbody>
-                {cli.documentos.map(doc => (
-                  <Operacion
-                    key={doc.tipodoc + doc.codigodoc}
-                    documento={doc}
-                  />
-                ))}
-              </tbody>
-              <tfoot></tfoot>
-            </table>
-          </td>
-        </tr>
-      </React.Fragment>
-   );
-}
- 
-export default Cliente;
+      <tr key={cli.codigo}>
+        <td>
+          <strong>{cli.codigo}</strong>
+        </td>
+        <td>
+          <em> {cli.rzs}</em>
+        </td>
+        <td className="numped">{cli.totalDocumentos}</td>
+        <td className="numped">
+          <button
+            className="btn btn-danger btn-sm"
+            onClick={() => onDelete(cli.codigo)}
+          >
+            borrar
+          </button>
+        </td>
+      </tr>
+      <tr key={cli.codigo + "-ops"}>
+        <td>&nbsp;</td>
+        <td colSpan="3">
+          <table className="table table-dark table-sm">
+            <thead></thead>
+            <tbody>
+              {cli.documentos.map(doc => (
+                <Operacion key={doc.tipodoc + doc.codigodoc} documento={doc} />
+              ))}
+            </tbody>
+            <tfoot></tfoot>
+          </table>
+        </td>
+      </tr>
+    </React.Fragment>
+  );
+};
 
+export default Cliente;
--- src/components/Operacion.jsx
+++ src/components/Operacion.jsx
@@ -1,11 +1,12 @@
-import React, { Component } from "react";
-const  Operacion = ({documento : doc}) => {
+import React from "react";
+// al ser una SFC no se requiere importar Component
+const Operacion = ({ documento: doc }) => {
   return (
-      <tr>
-        {doc.tipodoc} {doc.codigodoc} Fecha:
-        {doc.fechapedido.replace(" 0:00:00", "")} Ref: {doc.referencia}
-      </tr>
-    );
-}
- 
-export default  Operacion;
+    <tr>
+      {doc.tipodoc} {doc.codigodoc} Fecha:
+      {doc.fechapedido.replace(" 0:00:00", "")} Ref: {doc.referencia}
+    </tr>
+  );
+};
+
+export default Operacion;
