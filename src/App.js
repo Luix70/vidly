303c22a9431ad6cbfa4a02087b9da2fc3670f175
Luix70 <lortuno1@gmail.com>
2019-10-09 15:26:41 +0200

    Merged branches

--- src/App.js
+++ src/App.js
@@ -19,11 +19,7 @@ class App extends Component {
           totalFacturas="Incalculable"
         />
         <div className="row">
-<<<<<<< HEAD
           <div className="col-3"></div>
-=======
-          <div className="col-2"></div>
->>>>>>> 28b0551835070368ef3ff7ac903da707dd38efb7
           <div className="col">
             {" "}
             {this.state.resultConsulta.representantes.map(repre => (
