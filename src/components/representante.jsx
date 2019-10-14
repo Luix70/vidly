e4aa201bf68289b225c75c5a52d04094800c0b84
Luix70 <lortuno1@gmail.com>
2019-10-14 13:01:58 +0200

    representante otra vez

---
+++ src/components/representante.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import Cliente from "./cliente";
+import _ from "lodash";
+
+const Representante = ({ repres, onSort, sortColumn }) => {
+  const listaclientes = repres.clientes;
+  const ordenarPor = sortColumn.path;
+  const orden = sortColumn.order;
+
+  // console.log(
+  //   `ordenamos los clientes del representante ${nombreRepre} por la columna ${ordenarPor} (${orden})`
+  // );
+
+  const listaOrdenada = _.orderBy(listaclientes, [ordenarPor], [orden]);
+  return (
+    <React.Fragment>
+      <div className="row encab-representante">
+        <h3>{repres.nombre}</h3>
+      </div>
+      <table className="table table-dark ">
+        <thead className="encab-representante">
+          <tr>
+            <th onClick={() => onSort("codigo")}>
+              <span className="sortable">Cod</span>
+            </th>
+            <th onClick={() => onSort("rzs")}>
+              <span className="sortable">Cliente</span>
+            </th>
+            <th onClick={() => onSort("totalDocumentos")}>
+              <span className="sortable">Docs</span>
+            </th>
+            <th>&nbsp;</th>
+            {/* <th onClick={() => onSort("totalDocumentos")}>Importe</th> */}
+          </tr>
+        </thead>
+        <tbody>
+          {listaOrdenada.map(cli => (
+            <Cliente key={cli.codigo} cliente={cli} />
+          ))}
+        </tbody>
+        <tfoot>
+          <tr className="table-secondary">
+            <td colSpan="3">
+              {listaOrdenada.length === 0 && "No hay clientes "}
+              {listaOrdenada.length > 0 &&
+                "Hay " + listaOrdenada.length + " clientes"}
+            </td>
+
+            <td className="numped">&nbsp;</td>
+          </tr>
+        </tfoot>
+      </table>
+    </React.Fragment>
+  );
+};
+
+export default Representante;
