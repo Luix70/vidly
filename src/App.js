5af40dc79a29ae3dd50b0c7bbae353809c45a139
Luix70 <lortuno1@gmail.com>
2019-10-20 22:33:46 +0200

    rutas con parametros

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React, { Component } from "react";
-import {Route, Switch} from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/navBar";
-import AreaReservada from './components/areaReservada';
-import Blog from "./components/blog"
-import Home from "./components/home"
+import AreaReservada from "./components/areaReservada";
+import Blog from "./components/blog";
+import Home from "./components/home";
 class App extends Component {
   render() {
+    const rutaBlog = props => <Blog user="Luis" {...props} />;
+
     return (
       <main style={{ backgroundColor: "#ddd" }}>
         <NavBar />
         <div className="content">
           <Switch>
             <Route path="/ar" component={AreaReservada} />
-            <Route path="/blog" render={(props)=><Blog user = "Luis" {...props}/>} />
-            <Route path="/"  component={Home} />
+
+            <Route path="/blog/:mes?/:anno?" render={rutaBlog} />
+            
+            
+
+            <Route path="/" component={Home} />
           </Switch>
         </div>
-  
       </main>
     );
   }
--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,9 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import {Link} from "react-router-dom";
 class Blog extends Component {
-    state = {  }
-    render() { 
-        return ( <h1>Blog</h1> );
-    }
+  render() {
+    const { user: autor } = this.props;
+    const {mes,anno} = this.props.match.params
+    return (
+      <React.Fragment>
+        <h1>Blog de {autor}</h1>
+        <ul>
+            <li><Link to="/blog/enero">Enero</Link></li>
+            <li><Link to="/blog/febrero">Febrero</Link></li>
+            <li><Link to="/blog/marzo">Marzo</Link></li>
+            <li><span>...</span></li>
+            <li><Link to="/blog"><em>  Todos</em></Link></li>
+        </ul>
+        <h3>mes: {mes|| "todos"} - Año: {anno || "2019" }</h3>
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default Blog;
