84e131dce123ea7e7e16d654e68a95a3ab793a69
Luix70 <lortuno1@gmail.com>
2019-10-21 11:20:12 +0200

    implementado not-found

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/navBar";
 import AreaReservada from "./components/areaReservada";
 import Blog from "./components/blog";
 import Home from "./components/home";
+import NotFound from "./components/notFound";
+
 class App extends Component {
   render() {
     const rutaBlog = props => <Blog user="Luis" {...props} />;
@@ -16,7 +18,11 @@ class App extends Component {
           <Switch>
             <Route path="/ar" component={AreaReservada} />
             <Route path="/blog/:mes?/:anno?" render={rutaBlog} />
-            <Route path="/" component={Home} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/mensajes" to="/blog?sortBy=oldest&verified=true" />
+            <Route exact path="/" component={Home} />
+
+            <Redirect to="/not-found" />
           </Switch>
         </div>
       </main>
